512ce44d8bc347e23e260187f4ccdf675a33c989
kallelzied <kallelzied@live.com>
2018-08-07 11:50:59 +0200

    added copyright

--- best_practice_examples/concatenating_strings_bp.py
+++ best_practice_examples/concatenating_strings_bp.py
@@ -1,3 +1,25 @@
+"""concatenating_strings_bp.py: Giving an example of best practicing with concatenating strings."""
+
+__author__ = "Zied Kallel"
+__copyright__ = """
+
+    Copyright 2018 concatenating_strings_bp.py
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+
+"""
+__license__ = "Apache 2.0"
+
 # A list of soccer teams from all over the globe
 soccer_teams = ["ARSENAL", "NEWCASTLE", "MAN UTD", "MAN CITY", "BARCELONE", "REAL MADRID", "PSG", "AJAX", "PSV",
                 "ATLANTA", "BAYERN MUNICH", "DORTMUND", "AC MILAN", "LAZIO"]
--- best_practice_examples/multiple_state_variables_bp.py
+++ best_practice_examples/multiple_state_variables_bp.py
@@ -1,3 +1,26 @@
+"""multiple_state_variables_bp.py: Giving an example of best practicing with concatenating strings."""
+
+__author__ = "Zied Kallel"
+__copyright__ = """
+
+    Copyright 2018 multiple_state_variables_bp.py
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+
+"""
+__license__ = "Apache 2.0"
+
+
 def fibonacci_wrong_way(n):
     x = 0
     y = 1
